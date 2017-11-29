d21cd936aca757ef66052943d1ccfb1d407d4a1d
Georg Kothmeier <georg.kothmeier@roomle.com>
2017-11-29 10:30:18 +0100

    With fingerprint options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      enabled: true,
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'woff', 'woff2', 'ttf', 'ico', 'webmanifest', 'wasm', 'mem', 'svg'],
+      replaceExtensions: ['html', 'js', 'css', 'map', 'webmanifest']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
